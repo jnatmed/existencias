70d071dfbbc937366c040d895f96bf823537ddf2
jnatmed <juanmannat@gmail.com>
2023-10-20 12:53:50 -0300

    agregando automatizacion en calendario

--- app_calendar.js
+++ app_calendar.js
@@ -10,26 +10,44 @@ SelectGenerator.generateMonthOptions(monthSelect);
 // Genera opciones de año desde 2022 hasta 2026
 SelectGenerator.generateYearOptions(yearSelect, 2022, 2026);
 
-document.addEventListener('DOMContentLoaded', function() {
+function mostrarCalendario(){
     const calendarTable = document.getElementById('calendar');
+    const selectedMonth = monthSelect.value;
+    const selectedYear = yearSelect.value;
+
+    // Vacía la tabla antes de generar un nuevo calendario
+    calendarTable.innerHTML = '';
+
+    // Verifica si se seleccionó un mes y un año antes de generar el calendario
+    if (selectedMonth && selectedYear) {
+        const calendarGenerator = new CalendarGenerator();
+        const dateString = `${selectedMonth}-${selectedYear}`;
+        calendarGenerator.generateCalendar(dateString);
+    } else {
+        alert('Por favor, selecciona un mes y un año.');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    
     const monthSelect = document.getElementById('monthSelect');
     const yearSelect = document.getElementById('yearSelect');
-    const generateButton = document.getElementById('generateButton');
 
-    generateButton.addEventListener('click', function() {
-        const selectedMonth = monthSelect.value;
-        const selectedYear = yearSelect.value;
+    // Obtener la fecha actual
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
 
-        // Vacía la tabla antes de generar un nuevo calendario
-        calendarTable.innerHTML = '';
+    // Establecer las selecciones por defecto en los elementos <select>
+    monthSelect.value = currentMonth.toLowerCase();
+    yearSelect.value = currentYear;
+    mostrarCalendario();
 
-        // Verifica si se seleccionó un mes y un año antes de generar el calendario
-        if (selectedMonth && selectedYear) {
-            const calendarGenerator = new CalendarGenerator();
-            const dateString = `${selectedMonth}-${selectedYear}`;
-            calendarGenerator.generateCalendar(dateString);
-        } else {
-            alert('Por favor, selecciona un mes y un año.');
-        }
+    monthSelect.addEventListener('change', function() {
+        mostrarCalendario();
     });
+    yearSelect.addEventListener('change', function() {
+        mostrarCalendario();
+    });
+
 });
